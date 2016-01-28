0ed9c8c44bf8f4489d63688159c5f2f4b49001c9
Konrad Dzwinel <kdzwinel@gmail.com>
2016-01-28 12:30:05 +0100

    Display questions, notify about question change.

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,45 @@
 import User from './user.js';
+import QuestionRepository from './question-repository.js';
 
 const ref = new Firebase("https://datacontest.firebaseio.com");
 const user = new User(ref);
+let questionRepo = null;
 
 const $btn = document.getElementById('js-log-in');
 const $name = document.getElementById('js-user-name');
 const $avatar = document.getElementById('js-user-avatar');
 const $email = document.getElementById('js-user-email');
+const $questions = document.getElementById('js-questions');
 
 $btn.addEventListener('click', () => {
     user.authenticate().then(() => {
         $name.innerHTML = user.name;
         $email.innerHTML = user.email;
         $avatar.src = user.avatar;
+
+        questionRepo = new QuestionRepository(ref);
+
+        questionRepo.onReady(questions => {
+            questions.forEach((question, idx) => {
+                let li = document.createElement('li');
+                li.innerHTML = question.text;
+
+                $questions.appendChild(li);
+            });
+        });
+
+        questionRepo.onQuestionChange(questionIdx => {
+            console.log('qchange', questionIdx);
+            if(questionIdx === -1) {
+                return;
+            }
+
+            if(questionIdx - 1 >= 0) {
+                $questions.childNodes[questionIdx - 1].style.fontWeight = 'normal';
+            }
+
+            $questions.childNodes[questionIdx].style.fontWeight = 'bold';
+        });
     }).catch((error) => {
         alert(error);
     });
---
+++ js/question-repository.js
@@ -0,0 +1,46 @@
+class QuestionRepository {
+    constructor(firebase) {
+        this.firebase = firebase;
+        this.questions = null;
+        this.currentQuestion = null;
+
+        this._listeners = {
+            'ready': [],
+            'question-change': [],
+            'error': []
+        };
+
+        let questionsRef = this.firebase.child('/questions');
+        let currentQuestionRef = this.firebase.child('/current-question');
+
+        questionsRef.once("value", (snapshot) => {
+            this.questions = snapshot.val();
+            this._trigger('ready', this.questions);
+        }, this._trigger.bind('error'));
+
+        currentQuestionRef.on("value", (snapshot) => {
+            this.currentQuestion = snapshot.val();
+            this._trigger('question-change', this.currentQuestion);
+        }, this._trigger.bind('error'));
+    }
+
+    _trigger(action, data) {
+        (this._listeners[action]).forEach((callback) => {
+            callback(data);
+        });
+    }
+
+    onError(listener) {
+        this._listeners['error'].push(listener);
+    }
+
+    onReady(listener) {
+        this._listeners['ready'].push(listener);
+    }
+
+    onQuestionChange(listener) {
+        this._listeners['question-change'].push(listener);
+    }
+}
+
+export default QuestionRepository;
