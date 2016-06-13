4b34eda36770d09f7c50f7acf0be179a96d19dc2
Konrad Dzwinel <kdzwinel@gmail.com>
2016-06-13 11:15:57 +0200

    Remove hack forcing HTTPS as it's no longer needed

--- js/app.js
+++ js/app.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import SlideList from './components/slideList.js';
 
 function start() {
-    const isDev = (location.hostname === 'localhost' || location.hostname === '127.0.0.1');
-
-    // force HTTPS
-    if(!isDev && location.protocol !== 'https:') {
-        location.protocol = 'https:';
-    }
-
     ReactDOM.render(
         <SlideList/>,
         document.getElementById('app')
